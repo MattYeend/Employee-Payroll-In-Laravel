829c58fe8167d76a8b1fa9fd21e62d45d3283976
Matt Yeend <mattyeend1@hotmail.co.uk>
2021-03-13 14:04:21 +0000

    Models and Controllers change

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Payroll;
+use App\Employee;
+use App\Role;
+use App\Department;
+use Session;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -19,10 +24,14 @@ class HomeController extends Controller
     /**
      * Show the application dashboard.
      *
-     * @return \Illuminate\Contracts\Support\Renderable
+     * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        return view('home');
+        return view('home',['employees' => Employee::take(4)->get(),
+                            'employeesCount' =>Employee::count(),
+                            'payrolls'=>Payroll::take(4)->get(),
+                            'roles' => Role::count(),
+                            'departments' => Department::count()]);
     }
 }
